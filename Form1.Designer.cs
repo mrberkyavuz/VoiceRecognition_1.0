3eeb74f8044bba06bd83001b2669afed13873a8c
Berk Yavuz <berkyvz2001@gmail.com>
2023-06-07 20:45:49 +0300

    Dosyalar yüklendi

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace VoiceRecognition_1._0
+{
+    partial class LstCommands
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.TmrSpeaking = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(656, 492);
+            this.listBox1.TabIndex = 0;
+            // 
+            // TmrSpeaking
+            // 
+            this.TmrSpeaking.Enabled = true;
+            this.TmrSpeaking.Interval = 1000;
+            this.TmrSpeaking.Tick += new System.EventHandler(this.TmrSpeaking_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::VoiceRecognition_1._0.Properties.Resources.circlewave;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(656, 492);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LstCommands
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(656, 492);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Window;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "LstCommands";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer TmrSpeaking;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace VoiceRecognition_1._0
+{
+    public partial class LstCommands : Form
+    {
+
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer Claire = new SpeechSynthesizer();
+        SpeechRecognitionEngine startlistening = new SpeechRecognitionEngine();
+        Random rnd = new Random();
+        int RecTimeOut = 0;
+        DateTime TimeNow = DateTime.Now;
+
+        public LstCommands()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Default_SpeechRecognized);
+            _recognizer.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(_recognizer_SpeechRecognized);
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            startlistening.SetInputToDefaultAudioDevice();
+            startlistening.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            startlistening.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(startlistening_SpeechRecognized);
+        }
+
+
+        private void Default_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            int ranNum;
+            string speech = e.Result.Text;
+
+            if (speech == "Hello")
+            {
+                Claire.SpeakAsync("Hello, I am here");
+            }
+
+            if (speech == "How are you")
+            {
+                Claire.SpeakAsync("I am doing great ,how are you");
+            }
+
+            if (speech == "What time is it")
+            {
+                Claire.SpeakAsync(DateTime.Now.ToString("h mm tt"));
+            }
+            if (speech == "Stop listening") 
+            {
+
+                Claire.SpeakAsync("If you need me just ask");
+                _recognizer.RecognizeAsyncCancel();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            if (speech == "Open google") 
+            {
+                Claire.SpeakAsync("Okey Sir I'm opening Google now");
+                Process.Start("chrome", "https://www.google.com/");
+            }
+
+            if (speech == "Open youtube")
+            {
+                Claire.SpeakAsync("Okey Sir I'm opening Youtube now");
+                Process.Start("chrome", "https://www.youtube.com/?hl=tr&gl=TR");
+            }
+
+            if (speech == "Open whatsapp")
+            {
+                Claire.SpeakAsync("Okey Sir I'm opening Whatsapp now");
+                Process.Start("chrome", "https://web.whatsapp.com/");
+            }
+
+            if (speech == "Say hello to Enes")
+            {
+                Claire.SpeakAsync("Okey let's we say hello to Enes");
+                Process.Start("chrome", "https://web.whatsapp.com/send?phone=+905056721903&text=Hello%20Enes");
+            }
+
+            if (speech == "Say hello to Levent")
+            {
+                Claire.SpeakAsync("Okey let's we say hello to Levent");
+                Process.Start("chrome", "https://web.whatsapp.com/send?phone=+905332010820&text=Hello%20Levent");
+            }
+
+        }
+
+        private void _recognizer_SpeechRecognized(object sender, SpeechDetectedEventArgs e)
+        {
+            RecTimeOut = 0;
+        }
+
+        private void startlistening_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text;
+
+            if (speech == "Wake up")
+            {
+                startlistening.RecognizeAsyncCancel();
+                Claire.SpeakAsync("Yes I am here");
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+
+        private void TmrSpeaking_Tick(object sender, EventArgs e)
+        {
+            if (RecTimeOut == 10) 
+            {
+                _recognizer.RecognizeAsyncCancel();
+            }
+            else if (RecTimeOut == 11) 
+            {
+                TmrSpeaking.Stop();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+                RecTimeOut = 0;
+            }
+        }
+    }
+}
